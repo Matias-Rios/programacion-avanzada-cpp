6f7ea190b3b30cb12bb9fdc1dc8708db5a98ec19
Matias-Rios <69057584+Matias-Rios@users.noreply.github.com>
2026-09-16 08:39:56 -0500

    t2

--- laboratorios/semana-05-sesion-02/ejercicio1_puntero_colgante.cpp
+++ laboratorios/semana-05-sesion-02/ejercicio1_puntero_colgante.cpp
@@ -2,6 +2,7 @@
 
 // TODO: predice antes de compilar. ¿que advertencia esperas que de
 // el compilador sobre esta funcion?
+//Al ser letura una variable local cuando se termine de ejecutar la funcion, su direccion quedara apuntando a nada siendo un putero colgante
 int* obtenerLecturaInsegura(int valorSensor) {
     int lectura = valorSensor * 2;
     return &lectura;
